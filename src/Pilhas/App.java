3df21bbfb116b1c239df9d43d09c4b121b5b3f77
Vinicius Santos <viniciuspereira2244@gmail.com>
2022-01-17 12:14:51 -0300

    Segundo update do codigo

--- src/Pilhas/App.java
+++ src/Pilhas/App.java
@@ -1,15 +1,36 @@
 package Pilhas;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Pilha p = new Pilha();
-        p.empilhar("Portuguesa ");
-        p.empilhar("Frango com catupiry ");
-        p.empilhar("Calabresa ");
-        p.empilhar("Quatro queijos ");
-        p.empilhar(10);
+
+        Object n;
+        /*As expressões devem conter literais de
+        A a J, operadores (+ (adição), - (subtração), / (divisão), * (multiplicação), ^
+        (potenciação)) e delimitadores de escopo ( “(“, ”)”, ”[“, ”]”, ”{“, ”}” ).*/
+
+        int tm=0;
+        int cont=0;
+        System.out.println("Quantos Caracteres tem a expressão? ");
+        tm = input.nextInt();
+
+        while(cont<tm){
+            System.out.println("Digite: ");
+            n = input.next();
+            p.empilhar(n);
+            cont++;
+        }
+
+
         while (p.pilhaVazia() == false) {
             System.out.println(p.desempilhar());
         }
+
+//        while (p.pilhaVazia() == false) {
+//            System.out.println(p.desempilhar());
+//        }
     }
 }
